b695d2719be2acbc1be9800ca274f2ecd3779288
Anastasiia Paliha <nastya.paliha@gmail.com>
2024-12-17 20:46:45 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
 // write code here
+const ulLists = document.querySelectorAll('li');
+
+ulLists.forEach((el) => {
+  if (el.querySelector('ul')) {
+    const textNode = el.firstChild;
+    const containerElement = document.createElement('span');
+
+    textNode.parentNode.insertBefore(containerElement, textNode);
+    containerElement.appendChild(textNode);
+
+    containerElement.addEventListener('click', (e) => {
+      e.stopPropagation();
+
+      const childUl = el.querySelector('ul');
+
+      if (childUl) {
+        childUl.style.display =
+          childUl.style.display === 'none' ? 'block' : 'none';
+      }
+    });
+  }
+});
